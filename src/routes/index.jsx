733eeb962357857037a6755ae5899e34b68fed91
Armando C. Santisbon <3804335+santisbon@users.noreply.github.com>
2023-09-06 23:55:57 -0500

    Add instructions

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,10 +3,14 @@ export default function Index() {
     <>
       <p className="zero-state">
         Keep a list of Fediverse servers and view their custom emojis and other features like character limit 
-        and translation when available.
+        and translation when available. Curently supports Mastodon, Firefish, Pleroma, Iceshrimp, Friendica, Takahē, and Fedibird.
+        <br></br>You can find a server on
       <a href='https://instances.social/list/advanced#lang=&allowed=&prohibited=&min-users=20000&max-users=' 
       target='_blank' rel='noopener noreferrer'
-      className="zero-state"> Help me find a server</a>.
+      className="zero-state"> instances.social</a> or on
+      <a href='https://fedidb.org/network'
+      target='_blank' rel='noopener noreferrer'
+      className="zero-state"> fedidb.org</a>.
       </p>
       <p className="zero-state">
         Searching your list looks in the server name and any personal notes you add to it. 
