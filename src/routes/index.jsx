815a9625c327a7466348c21fb13ece2f6d06fea3
Armando C. Santisbon <3804335+santisbon@users.noreply.github.com>
2023-10-02 15:02:10 -0500

    Fix table

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -16,20 +16,25 @@ export default function Index() {
         Searching your list looks in the server name and any personal notes you add to it. 
         Tip: You can search for your favorites by typing "favorite".
       </p>
-      <p>
+      <p></p>
       <table className="center">
-        <tr>
-          <a href='https://ko-fi.com/E1E2OOCOY' target='_blank' rel='noopener noreferrer'>
-            <img height='36' className='ko-fi-logo' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' />
-          </a>
-        </tr>
-        <tr>
-          <a href="https://account.venmo.com/u/Armando-Santisbon" target='_blank' rel='noopener noreferrer'> 
-              <img src='/img/venmo.svg' className='venmo-logo' alt='venmo'></img>
-          </a>
-        </tr>
+        <tbody>
+          <tr>
+            <td>
+              <a href='https://ko-fi.com/E1E2OOCOY' target='_blank' rel='noopener noreferrer'>
+                <img height='36' className='ko-fi-logo' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' />
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <a href="https://account.venmo.com/u/Armando-Santisbon" target='_blank' rel='noopener noreferrer'> 
+                  <img src='/img/venmo.svg' className='venmo-logo' alt='venmo'></img>
+              </a>
+            </td>
+          </tr>
+        </tbody>
       </table>
-      </p>
     </>
   );
 }
