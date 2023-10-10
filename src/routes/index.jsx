2ac5939bdf1f3863639d976baa60add46b9f713d
Armando C. Santisbon <3804335+santisbon@users.noreply.github.com>
2023-10-10 16:34:44 -0500

    Update feature list

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,11 +5,11 @@ export default function Index() {
       <ul className="zero-state">
         <li>Keep a list of Fediverse servers and view their custom emojis (where available) and other features like character limit and translation. </li>
         <li>Click/tap to copy emojis.</li>
-        <li>Curently supports Mastodon, Firefish, Iceshrimp, Pleroma, Akkoma, Friendica, Takahē, Fedibird, Lemmy, Pixelfed, WordPress, Peertube, GotoSocial, GNUSocial.</li>
         <li>Searching your list looks in the server name and any personal notes you add to it.</li>
         <li>Tip: You can search for your favorites by typing "favorite".</li>
         <li>Install as an app on desktop and mobile for <a href="https://web.dev/learn/pwa/progressive-web-apps/#compatibility" target='_blank' rel='noopener noreferrer'>browsers that support it</a>.</li>
         <li>Shareable URLs.</li>
+        <li>Tested on Akkoma, Bookwyrm, Cherrypick, Fedibird, Firefish, Foundkey, Friendica, GNUSocial, GotoSocial, Hometown, Hubzilla, Iceshrimp, Kbin, Lemmy, Mastodon, Microdotblog, Misskey, Mobilizon, Owncast, Peertube, Pixelfed, Pleroma, Plume, Takahē, WordPress.</li>
       </ul>
       <p className="zero-state">
         You can look up a server on
