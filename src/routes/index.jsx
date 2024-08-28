89bd58fa13ec383abf09c1583c839c761b2f9825
Armando C. Santisbon <3804335+santisbon@users.noreply.github.com>
2024-08-27 19:53:15 -0500

    Home page text

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,10 @@
 export default function Index() {
   return (
     <>
+      <h2 className="zero-state"><a href='https://symbol.fediverse.info' target='_blank' rel='noopener noreferrer'>⁂</a> Fediverse Server Viewer</h2>
       <h3 className="zero-state">Features</h3>
       <ul className="zero-state">
-        <li>Keep a list of Fediverse servers and view their custom emojis (where available) and other features like character limit and translation. </li>
+        <li>Keep a list of Fediverse servers and view their custom emojis and other features like character limit and translation availability. </li>
         <li>Click/tap to copy emojis.</li>
         <li>Searching your list looks in the server name and any personal notes you add to it.</li>
         <li>Tip: You can search for your favorites by typing "favorite".</li>
@@ -12,11 +13,11 @@ export default function Index() {
         <li>Tested on Akkoma, Bookwyrm, Cherrypick, Fedibird, Firefish, Foundkey, Friendica, GNUSocial, GotoSocial, Hometown, Hubzilla, Iceshrimp, Kbin, Lemmy, Mastodon, Microdotblog, Misskey, Mobilizon, Owncast, Peertube, Pixelfed, Pleroma, Plume, Takahē, WordPress.</li>
       </ul>
       <p className="zero-state">
-        You can look up a server on
-          <a href='https://instances.social/list/advanced#lang=&allowed=&prohibited=&min-users=20000&max-users=' 
-          target='_blank' rel='noopener noreferrer'> instances.social</a> or on
+        Don't know what servers to add to your  list? You can find them on
           <a href='https://fedidb.org/network'
-          target='_blank' rel='noopener noreferrer'> fedidb.org</a>.
+          target='_blank' rel='noopener noreferrer'> fedidb.org</a> or on
+          <a href='https://instances.social/list/advanced#lang=&allowed=&prohibited=&min-users=20000&max-users=' 
+          target='_blank' rel='noopener noreferrer'> instances.social</a>.
       </p>
     </>
   );
